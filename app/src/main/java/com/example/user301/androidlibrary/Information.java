d9dd87d1ae36964ab52297943d8eab6ae0680d35
YakushevRoman <pegasus199197@gmail.com>
2019-02-01 13:49:04 +0200

    Initial commit

---
+++ app/src/main/java/com/example/user301/androidlibrary/Information.java
@@ -0,0 +1,27 @@
+package com.example.user301.androidlibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class Information extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information);
+
+        ListView listView = (ListView) findViewById(R.id.info_list_view);
+
+        String [] list_info = getResources().getStringArray(R.array.my_infarmation);
+
+        ArrayAdapter <String> stringArrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                list_info);
+
+        listView.setAdapter(stringArrayAdapter);
+
+    }
+}
